a12665586435d12e9e0086ecb8ce4d433e91ca36
zakaria tehami <zaki.tehami@gmail.com>
2024-02-09 17:25:26 +0100

    modification de testimonialController pour envoyer les avis OK

--- src/Controller/TestimonialController.php
+++ src/Controller/TestimonialController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Testimonial;
 use App\Repository\OpeningDayRepository;
 use App\Repository\TestimonialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TestimonialController extends AbstractController
 {
@@ -20,5 +23,32 @@ class TestimonialController extends AbstractController
             'testimonials' => $testimonials,
             'openingDays' => $openingDays,
         ]);
+    }
+    private $entityManager; // Ajoutez une propriété privée pour le gestionnaire d'entités
+
+    // Injectez le gestionnaire d'entités dans le constructeur
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    public function submitTestimonial(Request $request): Response
+    {
+        $pseudo = $request->request->get('pseudonym');
+        $content = $request->request->get('floatingTextarea2');
+        $note = $request->request->get('rating');
+
+        // Créer une nouvelle instance de l'entité Testimonial
+        $testimonial = new Testimonial();
+        $testimonial->setPseudo($pseudo);
+        $testimonial->setContent($content);
+        $testimonial->setNote($note);
+        $testimonial->setApproved(false); // L'avis n'est pas approuvé par défaut
+
+        // Enregistrer l'avis dans la base de données
+        $this->entityManager->persist($testimonial);
+        $this->entityManager->flush();
+
+        // Rediriger vers la page d'accueil (ou une autre page) après la soumission
+        return $this->redirectToRoute('app_home');
     }
 }
